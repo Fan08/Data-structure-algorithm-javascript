7dda7bb988efbbef236ec75b3bc038faab1b66ee
Fan08 <1009395048@qq.com>
2020-05-02 20:57:24 +0800

    2020-05-02

--- 06.快速排序.js
+++ 06.快速排序.js
@@ -4,37 +4,43 @@
  * @Author: 唐帆
  * @Date: 2020-05-01 19:12:42
  * @LastEditors: 唐帆
- * @LastEditTime: 2020-05-01 20:09:12
+ * @LastEditTime: 2020-05-02 20:57:13
  */
 
-/*
-失败，严重超时
-*/
-
 function quickSort(arr, low = 0, high = arr.length - 1) {
-    let mid = divide(arr, low, high);
     if (low >= high) return;
+    let mid = divide(arr, low, high);
+
+    // 递归至低位和高位重合
+    // 类似于二分法递归至粒度为 1 时
     quickSort(arr, low, mid - 1);
     quickSort(arr, mid + 1, high);
+    return arr
 }
 
 
 function divide(a, low, high) {
-    let k = a[low];
-    do {
-        console.log(low);
-        while (low < high && a[high] >= k) --high;
-        if (low < high) {
-            a[low] = a[high];
-            ++low;
-        }
-        while (low < high && a[low] <= k) ++low;
-        if (low > high) {
-            a[high] = a[low];
-            --high;
-        }
-    } while (low != high)
-    a[low] = k;
+    // 选取第一位为标准值
+    let key = a[low];
 
+    // 在 low（低位） 和 high（高位）不等的情况下进入循环
+    while (low < high) {
+        // 在低位小于高位，高位的值大于标准值时进入循环
+        while (key <= a[high] && low < high) {
+            high--;
+        };
+        a[low] = a[high]
+        a[high] = key;
+
+        // 在低位小于高位，低位的值小于标准值时进入循环
+        while (key >= a[low] && low < high) {
+            low++;
+        }
+        a[high] = a[low]
+        a[low] = key;
+    }
+
+    // 返回最终的中位
+    // 即低位和高位重合位
     return low;
 }
